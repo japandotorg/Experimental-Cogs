8e0b09c14d129bbd9403b20875a2595691a36c36
japandotorg <yash.kul69@gmail.com>
2024-06-24 04:39:12 +0530

    [Downloader] custom `pipinstall`

--- downloader/core.py
+++ downloader/core.py
@@ -1,22 +1,34 @@
-from typing import ClassVar, List, cast
+import asyncio
+import functools
+import subprocess
+from pathlib import Path
+from sys import executable
+from concurrent.futures import ThreadPoolExecutor
+from typing import Any, ClassVar, Iterable, List, Optional, cast
 
 import TagScriptEngine as tse
 
+import discord
 from redbot.core import commands
 from redbot.core.bot import Red
 from redbot.cogs.downloader.repo_manager import Repo
+from redbot.core.utils.menus import close_menu, menu, DEFAULT_CONTROLS
 from redbot.core.utils.chat_formatting import pagify, box
 from redbot.cogs.downloader.downloader import Downloader as _Downloader
+from redbot.cogs.downloader.repo_manager import ProcessFormatter
 
 from ._tagscript import RepoAdapter
 
 
 class Downloader(_Downloader):
-    __doc__ = _Downloader.__doc__
-
-    repo: commands.Group = cast(commands.Group, _Downloader.repo.copy())
+    __doc__: Optional[str] = _Downloader.__doc__
 
     __version__: ClassVar[str] = "1.0.0"
+
+    PIP_INSTALL: ClassVar[str] = "{python} -m pip install -U -t {target} {requirements}"
+
+    repo: commands.Group = cast(commands.Group, _Downloader.repo.copy())
+    _old_pip: commands.Command = cast(commands.Command, _Downloader.pipinstall.copy())
 
     def __init__(self, bot: Red) -> None:
         super().__init__(bot)
@@ -24,6 +36,8 @@ class Downloader(_Downloader):
         self.interpreter: tse.Interpreter = tse.Interpreter(
             [tse.LooseVariableGetterBlock()]
         )
+        self.__executor: ThreadPoolExecutor = ThreadPoolExecutor(1)
+        self.__lock: asyncio.Lock = asyncio.Lock()
 
     def format_help_for_context(self, ctx: commands.Context) -> str:
         pre_processed = super().format_help_for_context(ctx)
@@ -35,6 +49,59 @@ class Downloader(_Downloader):
             formatting, {"repo": RepoAdapter(repo)}
         )
         return output.body  # type: ignore
+
+    async def _run(
+        self, *args: Any, **kwargs: Any
+    ) -> subprocess.CompletedProcess[bytes]:
+        async with self.__lock:
+            process: (
+                subprocess.CompletedProcess
+            ) = await asyncio.get_running_loop().run_in_executor(
+                self.__executor,
+                functools.partial(
+                    subprocess.run,
+                    *args,
+                    stdout=subprocess.PIPE,
+                    stderr=subprocess.PIPE,
+                    **kwargs,
+                ),
+            )
+            return process
+
+    async def _pip(self, requirements: Iterable[str], target_dir: Path) -> str:
+        if not requirements:
+            raise commands.BadArgument("Requirements not found.")
+        process: subprocess.CompletedProcess[bytes] = await self._run(
+            ProcessFormatter().format(
+                self.PIP_INSTALL,
+                python=executable,
+                target=target_dir,
+                requirements=requirements,
+            )
+        )
+        return (
+            process.stdout.decode("utf-8").strip()
+            or process.stderr.decode("utf-8").strip()
+        )
+
+    @commands.is_owner()
+    @discord.utils.copy_doc(_old_pip)
+    @commands.command(require_var_positional=True)
+    async def pipinstall(self, ctx: commands.Context, *deps: str) -> None:
+        async with ctx.typing():
+            response: str = await self._pip(deps, self.LIB_PATH)
+        embeds: List[discord.Embed] = []
+        pages: List[str] = [p for p in pagify(response)]
+        for index, page in enumerate(pages):
+            embed: discord.Embed = discord.Embed(
+                description=box(page), color=await ctx.embed_color()
+            )
+            embed.set_footer(text="Page {}/{}".format(index + 1, len(pages)))
+            embeds.append(embed)
+        controls = (
+            {"\N{CROSS MARK}": close_menu} if len(embeds) == 1 else DEFAULT_CONTROLS
+        )
+        await menu(ctx, embeds, controls=controls, timeout=120.0)  # type: ignore
 
     repo.remove_command("list")
 
