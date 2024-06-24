40cfd0991f67b6e29044db8201b65e588d472350
japandotorg <yash.kul69@gmail.com>
2024-06-24 21:04:14 +0530

    [Downloader] use an ABC

--- downloader/common/utils.py
+++ downloader/common/utils.py
@@ -1,6 +1,7 @@
 import os
 import re
-from typing import ClassVar, Dict, List, Tuple
+from abc import ABC, abstractmethod
+from typing import Any, ClassVar, Dict, List, Tuple
 
 
 def humanize_required_cogs(data: Dict[str, str]) -> List[str]:
@@ -12,8 +13,7 @@ def humanize_required_cogs(data: Dict[str, str]) -> List[str]:
     return response
 
 
-# https://github.com/AAA3A-AAA3A/AAA3A_utils/blob/8ddc5c06be4d43cfabb96cc4cee1733581c38020/AAA3A_utils/cogsutils.py#L61-L86
-class ReplaceVars:
+class _ReplaceVars(ABC):
 
     ENV_VARS: ClassVar[Tuple[str, ...]] = (
         "USERPROFILE",
@@ -39,6 +39,30 @@ class ReplaceVars:
     def __str__(self) -> str:
         return self.content.strip()
 
+    @abstractmethod
+    def _replace(self) -> str:
+        raise NotImplementedError
+
+
+# https://github.com/AAA3A-AAA3A/AAA3A_utils/blob/8ddc5c06be4d43cfabb96cc4cee1733581c38020/AAA3A_utils/cogsutils.py#L61-L86
+class ReplaceVars(_ReplaceVars):
+
+    ENV_VARS: ClassVar[Tuple[str, ...]] = (
+        "USERPROFILE",
+        "HOME",
+        "USERNAME",
+        "COMPUTERNAME",
+    )
+
+    def __init__(
+        self, content: str, reverse: bool = False, replacement: bool = True
+    ) -> None:
+        self.content: str = content
+        self.reverse: bool = reverse
+        self.replacement: bool = replacement
+
+        self._replace()
+
     def replace(self) -> str:
         return self.content
 
@@ -46,7 +70,7 @@ class ReplaceVars:
         if not self.reverse:
             if not self.replacement:
                 return self.content
-            for env_var in ("USERPROFILE", "HOME", "USERNAME", "COMPUTERNAME"):
+            for env_var in self.ENV_VARS:
                 if env_var in os.environ:
                     regex = re.compile(re.escape(os.environ[env_var]), re.I)
                     self.content = regex.sub(f"{{{env_var}}}", self.content)
@@ -61,14 +85,16 @@ class ReplaceVars:
         else:
 
             class FakeDict(Dict):
+                def __init__(
+                    self, *args: Any, env_vars: Tuple[str, ...], **kwargs: Any
+                ) -> None:
+                    self.env_vars: Tuple[str, ...] = env_vars
+                    super().__init__(*args, **kwargs)
+
                 def __missing__(self, key: str) -> str:
-                    if (
-                        key.upper()
-                        in ("USERPROFILE", "HOME", "USERNAME", "COMPUTERNAME")
-                        and key.upper() in os.environ
-                    ):
+                    if key.upper() in self.env_vars and key.upper() in os.environ:
                         return os.environ[key.upper()]
                     return f"{{{key}}}"
 
-            self.content = self.content.format_map(FakeDict())
+            self.content = self.content.format_map(FakeDict(env_vars=self.ENV_VARS))
         return self.content
